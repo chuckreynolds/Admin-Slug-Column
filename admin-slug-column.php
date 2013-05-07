0d1696558e9f3f2e2413f4a62624f68c1cb7df60
chuckreynolds <chuck@rynoweb.com>
2013-05-07 02:26:57 -0700

    make wp repo official, push 0.2 to wordpress.org/extend/plugins/admin-slug-column/

--- admin-slug-column.php
+++ admin-slug-column.php
@@ -26,37 +26,41 @@
   along with this program. If not, see http://www.gnu.org/licenses/gpl-3.0.html
 */
 
-class wpAdminSlugColumn {
-  public function __construct() {
-    add_filter('manage_posts_columns', array($this, 'SAC_posts') );
-    add_action('manage_posts_custom_column', array($this, 'SAC_posts_data'), 10, 2);
-    add_filter('manage_pages_columns', array($this, 'SAC_pages') );
-    add_action('manage_pages_custom_column', array($this, 'SAC_pages_data'), 10, 2);
-  }
+if (!class_exists("wpAdminSlugColumn")) {
+  class wpAdminSlugColumn {
 
-  // Add slug to Posts column with option
-  public function SAC_posts($defaults) {
-    $defaults['SAC_Slug'] = __('Post Slug');
-    return $defaults;
-  }
-  public function SAC_posts_data($column_name, $id) {
-    if( $column_name == 'SAC_Slug' ) {
-      $post_slug = get_post($ID = $id)->post_name;
-      echo $post_slug;
+    public function __construct() {
+      add_filter('manage_posts_columns', array($this, 'SAC_posts') );
+      add_action('manage_posts_custom_column', array($this, 'SAC_posts_data'), 10, 2);
+      add_filter('manage_pages_columns', array($this, 'SAC_pages') );
+      add_action('manage_pages_custom_column', array($this, 'SAC_pages_data'), 10, 2);
     }
-  }
 
-  // Add slug to Pages column with option
-  public function SAC_pages($defaults) {
-    $defaults['SAC_Slug'] = __('Page Slug');
-    return $defaults;
-  }
-  public function SAC_pages_data($column_name, $id) {
-    if( $column_name == 'SAC_Slug' ) {
-        $page_slug = get_page($ID = $id)->post_name;
-        echo $page_slug;
+    // Add slug to Posts column with option
+    public function SAC_posts($defaults) {
+      $defaults['SAC_Slug'] = __('Post Slug');
+      return $defaults;
     }
-  }
+    public function SAC_posts_data($column_name, $id) {
+      if( $column_name == 'SAC_Slug' ) {
+        $post_slug = get_post($ID = $id)->post_name;
+        echo $post_slug;
+      }
+    }
 
-}
+    // Add slug to Pages column with option
+    public function SAC_pages($defaults) {
+      $defaults['SAC_Slug'] = __('Page Slug');
+      return $defaults;
+    }
+    public function SAC_pages_data($column_name, $id) {
+      if( $column_name == 'SAC_Slug' ) {
+          $page_slug = get_page($ID = $id)->post_name;
+          echo $page_slug;
+      }
+    }
+
+  } // end class wpAdminSlugColumn
+
+} // end check for existance of class wpAdminSlugColumn
 $wpAdminSlugColumn = new wpAdminSlugColumn();
--- readme.txt
+++ readme.txt
@@ -11,13 +11,18 @@ License URI: http://www.gnu.org/licenses/gpl-3.0.html
 Put the slug of post and page into the admin columns
 
 == Description ==
-Put the slug of post and page into the admin columns
+Simply put, this plugin adds the page and post slug into the admin columns.
+I built this quickly out of necessity to help identify pages and posts by their slug as sometimes the titles clients give them don't match up nicely with the url slug; so here's a fast way to do that. Nothing fancy, just does what it does.
 
 == Installation ==
-
+1. Upload the `admin-slug-column` directory to the `/wp-content/plugins/` directory
+1. Activate the Admin Slug Column plugin through the 'Plugins' menu in WordPress
+1. Go to Posts or Pages and see the column showing your slug
+1. Choose to hide/show the slug column in "Screen Options" tab up top
 
 == Frequently Asked Questions ==
-
+= Why build this? =
+Built quickly out of necessity to help identify pages and posts by their slug. Sometimes the titles clients give them don't match up nicely with the url slug; so here's a fast way to do it. Nothing fancy, just does what it does.
 
 == Screenshots ==
 
