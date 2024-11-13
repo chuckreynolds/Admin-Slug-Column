29c5e7a40a24ce2084baac58d7dd8f314a8caf9f
Chuck Reynolds <chuck@vuurr.com>
2024-11-13 09:16:02 -0700

    init refactor - untested stuff

--- admin-slug-column.php
+++ admin-slug-column.php
@@ -11,10 +11,10 @@
  * @wordpress-plugin
  * Plugin Name:       Admin Slug Column
  * Plugin URI:        https://github.com/chuckreynolds/Admin-Slug-Column
- * Description:       Adds the post url slug and page url path to the admin columns on edit screens.
- * Version:           1.6.1
+ * Description:       Adds the post URL slug and page URL path to the admin columns on edit screens.
+ * Version:           1.6.2
  * Requires at least: 5.2
- * Requires PHP:      7.2
+ * Requires PHP:      7.4
  * Author:            Chuck Reynolds
  * Author URI:        https://chuckreynolds.com
  * Text Domain:       admin-slug-column
@@ -29,77 +29,88 @@ if ( ! defined( 'WPINC' ) ) {
 
 // Only run plugin in the admin
 if ( ! is_admin() ) {
-	return false;
+	return;
 }
 
-Class WPAdminSlugColumn {
+/**
+ * Class WPAdminSlugColumn
+ */
+class WPAdminSlugColumn {
 
 	/**
-	* Constructor for WPAdminSlugColumn Class
-	*/
+	 * Constructor for WPAdminSlugColumn Class
+	 */
 	public function __construct() {
-		add_action( 'current_screen',             array( $this, 'WPASC_post_type' ) );
-		add_filter( 'manage_posts_columns',       array( $this, 'WPASC_posts' ) );
-		add_action( 'manage_posts_custom_column', array( $this, 'WPASC_posts_data' ), 10, 2 );
-		add_filter( 'manage_pages_columns',       array( $this, 'WPASC_posts' ) );
-		add_action( 'manage_pages_custom_column', array( $this, 'WPASC_posts_data' ), 10, 2 );
+		add_action( 'current_screen', [ $this, 'init' ] );
 	}
 
 	/**
-	 * Returns an object that includes the current screen's post type
+	 * Initialize the plugin
 	 *
-	 * @see https://developer.wordpress.org/reference/functions/get_current_screen/
+	 * @param WP_Screen $current_screen The current screen object.
 	 */
-	public function WPASC_post_type() {
-		return get_current_screen()->post_type;
+	public function init( $current_screen ) {
+		if ( ! $current_screen->base === 'edit' ) {
+			return;
+		}
+
+		add_filter( "manage_{$current_screen->post_type}_posts_columns", [ $this, 'add_column' ] );
+		add_action( "manage_{$current_screen->post_type}_posts_custom_column", [ $this, 'display_column' ], 10, 2 );
 	}
 
 	/**
 	 * Adds Slug column to Posts list column
 	 *
-	 * @param array $defaults An array of column names
+	 * @param array $columns An array of column names.
+	 * @return array Modified array of column names.
 	 */
-	public function WPASC_posts( $defaults ) {
-		$defaults['wpasc-slug'] = __( 'URL Path', 'admin-slug-column' );
-		return $defaults;
+	public function add_column( $columns ) {
+		$columns['wpasc-slug'] = __( 'URL Path', 'admin-slug-column' );
+		return $columns;
 	}
 
 	/**
-	 * Gets the post info from get_post function and displays the slug and/or path
+	 * Displays the slug and/or path in the custom column
 	 *
-	 * @param string $column_name Name of the column
-	 * @param int    $id          post id
-	 *
-	 * @see https://developer.wordpress.org/reference/functions/get_post/
+	 * @param string $column_name Name of the column.
+	 * @param int    $post_id     Post ID.
 	 */
-	public function WPASC_posts_data( $column_name, $id ) {
-		if ( $column_name == 'wpasc-slug' ) {
-			$post_info        = get_post( $id, 'string', 'display' );
-			$post_status      = $post_info->post_status;
-			$draft_slug_names = array( '%pagename%', '%postname%' );
+	public function display_column( $column_name, $post_id ) {
+		if ( 'wpasc-slug' !== $column_name ) {
+			return;
+		}
 
-			if ( 'draft' === $post_status || 'pending' === $post_status || 'future' === $post_status ) {
-				// unpublished status don't technically a slug yet so we have to use another function
-				$post_draft_url_array = get_sample_permalink( $id );
-				// grab the sample url path from the array and remove host and scheme
-				$post_draft_url_pre = str_replace( get_home_url(), '', $post_draft_url_array[0] );
-				// swap the draft %pagename% or %postname% holder with the sample permalink
-				$post_slug = str_replace( $draft_slug_names, $post_draft_url_array[1], $post_draft_url_pre );
-				// fyi: mb decoding is already done for us by the get_sample_permalink() array [1]
-				// now that we have the actual url path, because it's a draft lets make it gray
-				$post_slug = '<span style="color: #999;">' . $post_slug . '</span>';
-			} else {
-				// for published and everything else just use the post name and remove host and scheme
-				$post_slug = str_replace( get_home_url(), '', get_permalink( $id ) );
-				// decode for multibyte character support
-				$post_slug = esc_html( urldecode( $post_slug ) );
-			}
+		$post = get_post( $post_id );
+		$post_status = $post->post_status;
 
-			// output the slug
-			echo $post_slug;
+		if ( in_array( $post_status, [ 'draft', 'pending', 'future' ], true ) ) {
+			$this->display_draft_slug( $post_id );
+		} else {
+			$this->display_published_slug( $post_id );
 		}
 	}
 
+	/**
+	 * Displays the slug for draft, pending, or future posts
+	 *
+	 * @param int $post_id Post ID.
+	 */
+	private function display_draft_slug( $post_id ) {
+		$post_draft_url_array = get_sample_permalink( $post_id );
+		$post_draft_url_pre = str_replace( home_url(), '', $post_draft_url_array[0] );
+		$post_slug = str_replace( [ '%pagename%', '%postname%' ], $post_draft_url_array[1], $post_draft_url_pre );
+		echo '<span style="color: #999;">' . esc_html( $post_slug ) . '</span>';
+	}
+
+	/**
+	 * Displays the slug for published posts
+	 *
+	 * @param int $post_id Post ID.
+	 */
+	private function display_published_slug( $post_id ) {
+		$post_slug = str_replace( home_url(), '', get_permalink( $post_id ) );
+		echo esc_html( urldecode( $post_slug ) );
+	}
 }
 
-$WPAdminSlugColumn = new WPAdminSlugColumn();
+new WPAdminSlugColumn();
